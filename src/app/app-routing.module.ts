2cd456eeca59b8969836698649a8d9083060a53a
Guilherme Fonseca <guilhermao1234@hotmail.com>
2023-11-06 18:41:09 -0300

    feat: add route configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TelaInicialComponent } from './components/tela-inicial/tela-inicial.component';
+import { TelaListagemComponent } from './components/tela-listagem/tela-listagem.component';
+import { TelaCadastroComponent } from './components/tela-cadastro/tela-cadastro.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', component: TelaInicialComponent },
+  { path: 'lista', component: TelaListagemComponent },
+  { path: 'cadastro', component: TelaCadastroComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TelaInicialComponent } from './components/tela-inicial/tela-inicial.component';
+import { TelaListagemComponent } from './components/tela-listagem/tela-listagem.component';
+import { TelaCadastroComponent } from './components/tela-cadastro/tela-cadastro.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TelaInicialComponent,
+    TelaListagemComponent,
+    TelaCadastroComponent,
   ],
+  
   imports: [
     BrowserModule,
     AppRoutingModule
