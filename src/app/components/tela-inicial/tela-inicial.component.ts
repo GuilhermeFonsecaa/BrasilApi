2d20ea9791f8f1b3118c0ba1d1d55f040b22b2f5
Guilherme Fonseca <guilhermao1234@hotmail.com>
2023-11-08 12:39:25 -0300

    feat: add functionality to load contasBancarias from localStorage

--- src/app/components/tela-inicial/tela-inicial.component.ts
+++ src/app/components/tela-inicial/tela-inicial.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+export interface contaBancaria{
+agencia: string;
+bankCode: number;
+conta:string
+}
+
 @Component({
   selector: 'app-tela-inicial',
   templateUrl: './tela-inicial.component.html',
   styleUrls: ['./tela-inicial.component.css']
 })
-export class TelaInicialComponent {
 
+export class TelaInicialComponent {
+  contasBancarias: contaBancaria[] = [];
+
+  constructor() {
+    const contasBancarias = JSON.parse(localStorage.getItem('contasBancarias') || '{}');
+    this.contasBancarias = Object.values(contasBancarias);    
+  }
 }
