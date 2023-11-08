63d66119f042d33357b7663a18f02be74a9c17d8
Guilherme Fonseca <guilhermao1234@hotmail.com>
2023-11-08 13:22:15 -0300

    feat: add the functionality of displaying the list of API banks and selecting a specific bank

--- src/app/components/tela-listagem/tela-listagem.component.ts
+++ src/app/components/tela-listagem/tela-listagem.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BancoService } from 'src/app/services/banco.service';
+
+export interface Bank {
+  ispb: string;
+  name: string;
+  code: number;
+  fullName: string;
+}
 
 @Component({
   selector: 'app-tela-listagem',
   templateUrl: './tela-listagem.component.html',
   styleUrls: ['./tela-listagem.component.css']
 })
-export class TelaListagemComponent {
+
+export class TelaListagemComponent implements OnInit {
+  constructor(private bancoService: BancoService, private router: Router) { }
+  bankCode: string = '';
+  banks: Bank[] = [];
+  selectedBank: Bank | null = null;
+  showError: boolean = false;
+
+  ngOnInit(): void {
+    this.getBank();
+    this.getBankCode();
+  }
+
+
+  getBank(): void {
+    this.bancoService.getBank().subscribe((data: Bank[]) => {
+      this.banks = data;
+      this.selectedBank = null;
+      console.log(data);
+    })
+  }
+
+  getBankCode(): void {
+    this.bancoService.getBankCode(this.bankCode).subscribe((data: Bank) => {
+      this.selectedBank = data;
+      console.log(data)
+    })
+  }
+
+  registrationRoute(bankCode: number) {
+    this.router.navigate([`cadastro/${bankCode}`])
+  }
 
 }
+
